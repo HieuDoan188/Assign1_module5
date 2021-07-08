76fe55e0dd11d0e1c0876be12ceb48081371b13e
HieuDoan188 <dmhieu93@gmail.com>
2021-07-08 10:03:07 +0700

    commit

---
+++ lastname_firstname_grade_the_exams.py
@@ -0,0 +1,13 @@
+"""
+Task 1:
+
+Tạo một chương trình Python mới có tên “lastname_firstname_grade_the_exams.py.” 
+(Đảm bảo tệp mã nguồn của bạn nằm trong cùng thư mục với tệp dữ liệu bạn vừa tải xuống.)
+Tiếp theo, viết một chương trình cho phép người dùng nhập tên của một tệp. Cố gắng mở 
+tệp được cung cấp để truy cập đọc. Nếu tệp tồn tại, bạn có thể in ra một thông báo xác nhận. 
+Nếu tệp không tồn tại, bạn nên cho người dùng biết rằng không thể tìm thấy tệp và nhắc lại họ.
+Sử dụng try/except để thực hiện việc này (đừng chỉ sử dụng một loạt câu lệnh “if” — chúng
+tôi muốn chương trình này càng “chung chung” càng tốt). Bạn sẽ không nhận được điểm cho phần 
+này của chương trình nếu bạn viết một cái gì đó như sau để xác định các tệp dữ liệu hợp lệ:
+"""
+
